a23d0ce9f1bdd536d0d1a1d0d74c8484418b536a
PlatonBelogorcev <pbelogorcev@yandex.ru>
2023-07-20 20:51:45 +0300

    release v1

---
+++ README.md
@@ -0,0 +1,101 @@
+# Ball Speed Measurement Program
+
+This is the technical documentation for the "Ball Speed Measurement" program written in Python. The program is designed to recognize balls in a video, measure their speed before and after a collision, and display the results in real-time. The program uses the OpenCV library for image processing, PyQt5 for the user interface, and matplotlib for data visualization.
+
+## Requirements
+- Python 3.x
+- OpenCV
+- NumPy
+- Matplotlib
+- PyQt5
+
+## Installation
+1. Make sure you have Python 3.x installed on your system.
+2. Install the required libraries using the following command:
+   ```
+   pip install opencv-python numpy matplotlib PyQt5
+   ```
+
+## Usage
+1. Clone or download the repository from GitHub.
+2. Open the terminal or command prompt and navigate to the program's directory.
+3. Run the program using the following command:
+   ```
+   python ball_speed_measurement.py
+   ```
+4. The application window will open, showing the video feed and the measured ball speeds.
+
+## Program Description
+
+The program consists of two main classes:
+
+### 1. PetangBall Class
+This class represents a petang ball and handles its speed calculations.
+
+#### Attributes
+- `radius`: The radius of the petang ball.
+- `prev_position`: The previous position of the petang ball.
+- `object_diameter`: The assumed diameter of the petang ball in pixels.
+- `object_real_size`: The actual size of the petang ball in meters.
+- `pixel_to_meter`: Conversion factor from pixels to meters.
+- `speed`: The current speed of the petang ball in meters per second.
+- `speed_pixels_x`: The current speed of the petang ball in the x-axis in pixels per second.
+- `speed_pixels_y`: The current speed of the petang ball in the y-axis in pixels per second.
+- `speed_meters_x`: The current speed of the petang ball in the x-axis in meters per second.
+- `speed_meters_y`: The current speed of the petang ball in the y-axis in meters per second.
+- `max_speed`: The maximum speed recorded for the petang ball.
+- `speed_array`: An array to store the speed values over time.
+- `time_array`: An array to store the corresponding time values.
+
+#### Methods
+- `move`: Calculate the speed of the ball based on the change in position and elapsed time.
+- `includes`: Check if the petang ball includes another ball (used to detect collisions).
+- `label_text`: Return the speed information as a list of strings for display.
+
+### 2. PetangStorage Class
+This class is used to store and manage multiple petang balls.
+
+#### Attributes
+- `objects`: A list of PetangBall objects representing the detected balls.
+- `last_petan`: The last detected petang ball (used to compare and track changes).
+- `last_frame`: The timestamp of the last frame processed.
+
+#### Methods
+- `update_contours`: Update the ball positions and speeds based on the detected contours in the current frame.
+- `draw_arrows`: Draw arrows representing the ball's speed direction on the frame.
+- `draw_labels`: Draw speed labels on the frame.
+
+### 3. SpeedMeasurementApp Class
+This class is responsible for the user interface and video processing.
+
+#### Attributes
+- `speed_array`: An array to store the ball speeds over time.
+- `time_array`: An array to store the corresponding time values.
+- `min_radius`: Minimum radius of a ball to be considered.
+- `max_radius`: Maximum radius of a ball to be considered.
+- `timer_ticks`: Interval for frame processing (in milliseconds).
+- `storage`: An instance of the PetangStorage class for managing petang balls.
+
+#### Methods
+- `init`: Initialize the user interface and setup camera parameters.
+- `setup_ui`: Create the main application window and buttons.
+- `plot_speed_graph`: Plot the ball speed graph using Matplotlib.
+- `setup_camera`: Open the video capture and set up the QTimer for frame processing.
+- `find_black_circles`: Find black circles in the frame (used for collision detection).
+- `find_petan_circles`: Find petang balls in the frame using color segmentation.
+- `find_ball_circles`: Find the balls that meet the specified radius criteria.
+- `crop_frame`: Crop the frame to the specified dimensions.
+- `update_frame`: Process the frames, detect balls, and update the display.
+- `start_measurement`: Start the speed measurement.
+- `reset_measurement`: Reset the video capture and measurement data.
+- `resetspeed_measurement`: Reset the speed measurement data.
+- `pause_measurement`: Pause or resume the speed measurement.
+- `closeEvent`: Handle the event when the application is closed, releasing the video capture and displaying the speed graph.
+
+## Limitations and Improvements
+- The program's accuracy might be affected by various factors such as lighting conditions, ball surface, and camera quality.
+- The program assumes that there are only two types of balls (black and petang) and may not work well with different ball types.
+- To improve accuracy, you can calibrate the program using known ball speeds and sizes.
+- The user interface could be enhanced to provide more features and options, such as selecting video files, adjusting settings, and exporting data.
+
+Note: This documentation assumes basic familiarity with Python, OpenCV, and PyQt5. If you encounter any issues or have questions about the program, please refer to the source code and feel free to ask for further assistance.
---
+++ main.py
@@ -0,0 +1,328 @@
+import cv2
+import numpy as np
+import math
+import time
+import sys
+import matplotlib
+matplotlib.use('Qt5Agg')
+from matplotlib import pyplot as plt
+from PyQt5.QtCore import Qt, QTimer
+from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QVBoxLayout, QWidget
+
+petan_radius = 37
+black_radius = 90
+petan_size = 0.072
+black_size = 0.09
+dtr = 1.5
+
+crx,cry = 0.25, 0.2 # Crop parameters - less numbers => less crop
+fname = "Videos/example_video_1.mp4" # Choose your video here
+#fname = "Videos/example_video_2.mp4"
+
+
+class PetangBall:
+    def __init__(self, radius, position=None):
+        self.radius = radius
+        self.prev_position = position
+
+        self.object_diameter = petan_radius  # Assume the diameter of the object in pixels
+        self.object_real_size = petan_size  # The actual size of the object in meters
+        self.pixel_to_meter = self.object_real_size / self.object_diameter
+        self.speed = 0
+
+        self.speed_pixels_x = 0
+        self.speed_pixels_y = 0
+            
+
+        self.speed_meters_x = 0
+        self.speed_meters_y = 0
+
+        self.max_speed = 0
+        self.speed_array = []
+        self.time_array = []
+
+    
+    def move(self, current_time, elapsed_time, position):
+        if self.prev_position is not None:
+            ds = np.array(position) - np.array(self.prev_position)
+            ds_meters = ds * self.pixel_to_meter
+            dx, dy = ds
+            dx_meters, dy_meters = ds_meters
+                           
+            distance = np.linalg.norm(ds)
+            distance_meters = distance * self.pixel_to_meter
+
+            self.speed = distance_meters / elapsed_time
+            self.speed_array.append(self.speed)
+            self.time_array.append(current_time)
+
+
+            self.speed_pixels_x = dx / elapsed_time
+            self.speed_pixels_y = dy / elapsed_time
+            
+
+            self.speed_meters_x = dx_meters / elapsed_time
+            self.speed_meters_y = dy_meters / elapsed_time
+
+
+
+            if self.speed > self.max_speed:
+                self.max_speed = self.speed
+
+        self.prev_position = position
+
+    def includes(self, petan):
+        x = petan.prev_position[0] - self.prev_position[0]
+        y = petan.prev_position[1] - self.prev_position[1]
+        radius = x ** 2 + y ** 2
+        r2 = math.sqrt(radius)
+        if r2 < self.radius *dtr:
+            return True
+        return False
+
+
+    def label_text(self):
+        return [f"Speed: {self.speed:.2f} m/s",f"Speed X:{self.speed_meters_x:.2f}",f"Speed Y: {self.speed_meters_y:.2f}"]
+
+
+class PetangStorage:
+    def __init__(self):
+        self.objects = []
+        self.last_petan = None
+        self.last_frame = 0
+
+    def update_contours(self,current_frame, contours):
+        elapsed_time = (current_frame - self.last_frame)/1000
+
+        for c in contours:
+            ((x, y), radius) = cv2.minEnclosingCircle(c)
+            petan = PetangBall(radius, (x, y))
+
+            found = False
+            for obj in self.objects:
+                if obj.includes(petan):
+                    found = True
+                    # If the center lies in at least one known ball, then this is the same ball
+                    obj.move(current_frame/1000, elapsed_time, petan.prev_position)
+                    break
+
+            if not found:
+                self.objects.append(petan)
+
+        for obj in self.objects:
+            found = False
+            if len(self.objects) <= 2:
+                continue
+            for c in contours:
+                ((x, y), radius) = cv2.minEnclosingCircle(c)
+                petan = PetangBall(radius, (x, y))
+                if petan.includes(obj):
+                    found = True
+                    break
+
+            if not found:
+                self.objects.remove(obj)
+
+        self.last_frame = current_frame
+
+    def draw_arrows(self, frame):
+
+        for o in self.objects:
+            x,y = int(o.prev_position[0]), int(o.prev_position[1])
+    
+            vx_position = (int(o.prev_position[0] + o.speed_pixels_x), int(o.prev_position[1]),)
+            vy_position = (int(o.prev_position[0]), int(o.prev_position[1] + o.speed_pixels_y),)
+            
+            cv2.arrowedLine(frame, (x, y), vx_position, (0, 0, 255) , 5) 
+            cv2.arrowedLine(frame, (x, y), vy_position, (255, 0, 0) , 5) 
+    def draw_labels(self, frame):
+        if len(self.objects) > 2:
+            print(f"draw {len(self.objects)} labels")
+
+        for o in self.objects:
+            x,y = int(o.prev_position[0]), int(o.prev_position[1])
+            texts = o.label_text()
+            for i in range(len(texts)):
+                text = texts[i]
+                cv2.putText(frame, text, (x + 100, y + i * 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+class SpeedMeasurementApp(QWidget):
+    def init(self):
+        self.speed_array = []
+        self.time_array = []
+        self.min_radius = 20
+        self.max_radius = 100
+        self.timer_ticks = 60
+        self.setup_ui()
+        self.setup_camera()
+
+        self.storage = PetangStorage()
+
+    def setup_ui(self):
+        self.speed_label = QLabel("Ball Speed Meter")
+        self.image_label = QLabel()
+        self.pause_button = QPushButton("Pause")
+        self.reset_button = QPushButton("Reset")
+        self.resetspeed_button = QPushButton("Reset Speed")
+        self.quit_button = QPushButton("Quit")
+
+        self.quit_button.setStyleSheet("background-color: #BF5730")
+        self.pause_button.setStyleSheet("background-color: #BF8130")
+        self.resetspeed_button.setStyleSheet("background-color: #AA2A53")
+        self.setStyleSheet("background-color: #218457")
+
+        self.pause_button.clicked.connect(self.pause_measurement)
+        self.reset_button.clicked.connect(self.reset_measurement)
+        self.resetspeed_button.clicked.connect(self.resetspeed_measurement)
+        self.quit_button.clicked.connect(self.close)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.speed_label)
+        layout.addWidget(self.image_label)
+        layout.addWidget(self.reset_button)
+        layout.addWidget(self.resetspeed_button)
+        layout.addWidget(self.pause_button)
+        layout.addWidget(self.quit_button)
+
+        self.setLayout(layout)
+        self.setWindowTitle("Speed Measurement")
+        self.show()
+
+    def plot_speed_graph(self):
+        plt.figure()
+        for p in self.storage.objects:
+            plt.plot(p.time_array, p.speed_array)
+        plt.xlabel('Time')
+        plt.ylabel('Speed')
+        plt.title('Speed vs Time')
+        plt.show()
+
+    def setup_camera(self):
+        self.cap = cv2.VideoCapture(fname)
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.update_frame)
+        self.timer.start(self.timer_ticks)
+
+        self.prev_position = None
+        self.speed = 0
+        self.max_speed = 0
+        self.real_time_speed = 0
+        self.pause_measurement_flag = False
+
+    def find_black_circles(self, frame):
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        blur = cv2.GaussianBlur(gray, (5, 5), 0)
+        _, threshold = cv2.threshold(blur, 60 , 255, cv2.THRESH_BINARY_INV)
+        contours_, _ = cv2.findContours(threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        return contours_
+
+    def find_petan_circles(self, frame):
+        sat = cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)[:,:,1]
+        blur = cv2.GaussianBlur(sat, (3, 3), 0)
+        mask = 255 - cv2.threshold(blur, 100 , 255, cv2.THRESH_BINARY_INV)[1]
+        kernel = np.ones((15,15), np.uint8) 
+        mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+        contours_, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        return contours_
+        
+    def find_ball_circles(self, contours_, frame):
+        
+        contours = []
+
+        for c in contours_:
+            ((x, y), radius) = cv2.minEnclosingCircle(c)
+            if radius > self.max_radius or radius < self.min_radius:
+                continue
+
+            center = (int(x),int(y))
+            cv2.circle(frame,center,int(radius),(0,255,0),2)
+            contours.append(c)
+
+        return contours
+
+    def crop_frame(self, frame, dx=0.1, dy=0.1):
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        height, width, channel = frame.shape
+        crop_height = int(height * dx)
+        crop_width = int(width * dy)
+        crop_height_to = height - crop_height
+        crop_width_to = width-crop_width
+        return frame[crop_height:crop_height_to, crop_width:crop_width_to]
+        
+    def update_frame(self):
+        if not self.timer.isActive():
+            return
+
+        if self.pause_measurement_flag:
+            return
+
+        ret, frame = self.cap.read()
+        if not ret:
+            return
+        current_frame = self.cap.get(cv2.CAP_PROP_POS_MSEC); # Get current frame number
+
+        frame = self.crop_frame(frame, crx, cry)
+        contours_ = self.find_black_circles(frame) + self.find_petan_circles(frame)
+        contours = self.find_ball_circles(contours_, frame)
+        
+        self.storage.update_contours(current_frame, contours)
+
+        # Converting an image from OpenCV to Qt
+        self.storage.draw_arrows(frame)
+        frame = cv2.resize(frame, (800, 800))
+        self.storage.draw_labels(frame)
+        
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        height, width, channel = image.shape
+        bytes_per_line = 3 * width
+        q_image = QImage(image.data, width, height, bytes_per_line, QImage.Format_RGB888)
+        pixmap = QPixmap.fromImage(q_image)
+        self.image_label.setPixmap(pixmap)
+        self.real_time_speed = self.speed
+        self.last_frame  = current_frame
+
+    def start_measurement(self):
+        self.max_speed = 0
+        self.time_start = time.time()
+
+    def reset_measurement(self):
+        self.setup_camera()
+        self.prev_position = None
+        self.speed = 0
+        self.max_speed = 0
+
+        self.timer.start(self.timer_ticks)
+        self.pause_measurement_flag = False
+        self.pause_button.setText("Pause")
+
+        self.time_array = []
+        self.speed_array = []
+
+
+    def resetspeed_measurement(self):
+        self.speed = 0
+        self.max_speed = 0
+        self.time_array = []
+        self.speed_array = []
+
+    def pause_measurement(self):
+        if self.timer.isActive():
+            self.timer.stop()
+            self.pause_measurement_flag = True
+            self.pause_button.setText("Resume")
+        else:
+            self.timer.start(self.timer_ticks)
+            self.pause_measurement_flag = False
+            self.pause_button.setText("Pause")
+
+    def closeEvent(self, event):
+        self.cap.release()
+        cv2.destroyAllWindows()
+        self.plot_speed_graph() # Calling the matplotlib chart
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = SpeedMeasurementApp()
+    window.init()
+    sys.exit(app.exec_())
